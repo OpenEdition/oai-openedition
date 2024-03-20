bfc3976a39bc6f1a142ec7a517d189d28c5752e3
Jean-François Rivière <jean-francois.riviere@openedition.org>
2024-03-20 10:33:37 +0100

    upd conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx-rtd-theme"
 
 
 # Theme options are theme-specific and customize the look and feel of a theme
